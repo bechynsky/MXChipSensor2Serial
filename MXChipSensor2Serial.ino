64f7b3d7c90ec3b1542b4c1d20eb6111227f804a
Stepan Bechynsky <stepan@bechynsky.cz>
2019-11-27 09:39:26 +0100

    Added more sensors and output as JSON

--- MXChipSensor2Serial.ino
+++ MXChipSensor2Serial.ino
@@ -15,14 +15,19 @@
  */
 
 #include "HTS221Sensor.h"
+#include "LSM6DSLSensor.h"
 #include <OledDisplay.h>
 
 
 DevI2C *i2c;
-HTS221Sensor *sensor;
+HTS221Sensor *sensorTH;
+LSM6DSLSensor *sensorAG;
 float humidity = 0;
 float temperature = 0;
 unsigned char id;
+
+int accelerometerAxes[3]; // [0]=X [1]=Y [2]=XZ
+int gyroscopAxes[3]; // [0]=X [1]=Y [2]=Z
 
 
 void setup() {
@@ -30,32 +35,48 @@ void setup() {
   Screen.init();
 
   i2c = new DevI2C(D14, D15);
-  sensor = new HTS221Sensor(*i2c);
+  sensorTH = new HTS221Sensor(*i2c);
+  sensorAG = new LSM6DSLSensor(*i2c, D4, D5);
+  
+  sensorTH -> init(NULL);
+  sensorAG -> init(NULL);
 
-  sensor -> init(NULL);
+  sensorAG->enableAccelerator();
+  sensorAG->enableGyroscope();
+
+  // enable
+  sensorTH -> enable();
 }
 
 void loop() {
-  // enable
-  sensor -> enable();
+   sensorAG->resetStepCounter();
+            sensorAG->getXAxes(accelerometerAxes);
+            sensorAG->getGAxes(gyroscopAxes);
+
+  Serial.print("{");
+  Serial.printf("\"ax\":%d,\"ay\":%d,\"az\":%d", accelerometerAxes[0], accelerometerAxes[1], accelerometerAxes[2]);
+  Serial.print(",");
+  Serial.printf("\"gx\":%d,\"gy\":%d,\"gz\":%d", gyroscopAxes[0], gyroscopAxes[1], gyroscopAxes[2]);
+  Serial.print(",");
   
   // Get temperature
-  sensor -> getTemperature(&temperature);
-  Serial.print(temperature);
-  
-  Serial.print(";");
+  sensorTH -> getTemperature(&temperature);
+  Serial.printf("\"t\":%.1f",temperature);
+  Serial.print(",");
 
   // Get humidity
-  sensor -> getHumidity(&humidity);
-  Serial.println(humidity);
+  sensorTH -> getHumidity(&humidity);
+  Serial.printf("\"h\":%.1f",humidity);
+  Serial.println("}");
   
   // disable the sensor
-  sensor -> disable();
+  //sensorTH -> disable();
   // reset
-  sensor -> reset();
+  //sensorTH -> reset();
 
-  char buf[100];
-  sprintf(buf, "H: %.1f\nT: %.1f", humidity, temperature);
-  Screen.print(buf, true);
-  delay(1000);
+  //char buf[300];
+  //sprintf(buf, "H: %.1f\nT: %.1f", humidity, temperature);
+  //sprintf(buf, "%d\n%d\n%d", accelerometerAxes[0], accelerometerAxes[1], accelerometerAxes[2]);
+  //Screen.print(buf, true);
+  delay(50);
 }
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # MXChip Sensor to Serial port
 
-Sample application for [MXChip](https://microsoft.github.io/azure-iot-developer-kit/docs/get-started/) sending data from temperature and humidity sensor to serial port in CSV format.
+Sample application for [MXChip](https://microsoft.github.io/azure-iot-developer-kit/docs/get-started/) sending data from temperature, humidity, accelerometer and gyro sensors to serial port in JSON format.
 
 ```
-27.2;46.8
+{"ax":-193,"ay":-965,"az":-158,"gx":-1470,"gy":-980,"gz":-420,"t":24.0,"h":45.5}
 ```
 
 Application is designed to be used with [SerialToAzure](https://github.com/bechynsky/SerialToAzure) to demonstrate usage of Filed gateway in [Azure IoT Reference architecture](http://download.microsoft.com/download/A/4/D/A4DAD253-BC21-41D3-B9D9-87D2AE6F0719/Microsoft_Azure_IoT_Reference_Architecture.pdf).
